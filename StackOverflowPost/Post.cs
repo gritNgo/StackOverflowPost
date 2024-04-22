d0d81bb6b9a7097f763de69d3125ad82bb7a1b20
gritNgo <dustspeck00@gmail.com>
2024-04-22 14:22:56 +0200

    Add project files.

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public int VoteCount { get; private set; }
+
+        public void UpVote(string upVote) 
+        {
+            VoteCount++;
+        }
+
+        public void DownVote(string downVote)
+        {
+            VoteCount--;
+        }
+
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,42 @@
+﻿using StackOverflowPost;
+
+Post post = new Post();
+
+    Console.Write("Enter post title: ");
+    post.Title = Console.ReadLine();
+    Console.Write("Enter post description: ");
+    post.Description = Console.ReadLine();
+
+    post.CreatedAt = DateTime.Now;
+    Console.WriteLine($"Created on: {post.CreatedAt}");
+
+while (true)
+{
+    Console.Write("Enter: 'up' to Upvote, 'down' to downvote or 'exit' to exit: ");
+    var input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+    {
+        Console.WriteLine("Exited");
+        break;
+    }
+
+    else if (input.ToLower() == "up")
+    {
+        post.UpVote(input);
+        Console.WriteLine($"Votes: {post.VoteCount}");
+    }
+    else if (input.ToLower() == "down")
+    {
+        post.DownVote(input);
+        Console.WriteLine($"Votes: {post.VoteCount}");
+
+    }
+
+    else
+    {
+        Console.WriteLine("Invalid input");
+        continue;
+    }
+
+}
